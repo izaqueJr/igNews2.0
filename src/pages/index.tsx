c4051f380a12762627a98942e6365c96ea2ca720
Izaque Rodrigues <izaquesjunior@gmail.com>
2022-04-16 19:05:20 -0300

    inital comit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className={ styles.title }>Hello Next.js</h2>
+    </div>
+  )
+}
