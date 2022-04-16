8535728922061a315a02aaeb977147426b175101
Izaque Rodrigues <izaquesjunior@gmail.com>
2022-04-16 19:27:34 -0300

    configurações globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head >
+        <title>Inicio | Ig.News</title>
+      </Head>
       <h2 className={ styles.title }>Hello Next.js</h2>
-    </div>
+    </>
   )
 }
