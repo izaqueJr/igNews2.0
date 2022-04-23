ba45d1cbf9da743c605e451131cebac9738cacda
Izaque Rodrigues <izaquesjunior@gmail.com>
2022-04-23 19:21:51 -0300

    SAVE ON DATBASE

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+import { query as  q } from "faunadb";
+import GithubProvider from "next-auth/providers/github";
+
+import {fauna} from "../../../services/fauna";
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+
+    async signIn({ user, account, profile }) {
+      const {email} = user
+      await fauna.query(
+        q.Create(
+          q.Collection("users"),
+           { data: { email }}
+        ) 
+      )
+      console.log(user, account, profile)
+      return true
+    },
+  },
+})
