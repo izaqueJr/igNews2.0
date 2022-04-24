04e9b93e6a3bd7cb38891edd0d6a9e0b61187a6e
Izaque Rodrigues <izaquesjunior@gmail.com>
2022-04-24 18:28:15 -0300

    config strie webhooks

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import styles from './styles.module.scss' 
-
+import { useSession, signIn } from 'next-auth/react';
+import { getStripeJs } from "../../services/stripe.js";
+import { api } from './../../services/api';
 interface SubsribeButtonProps {
     priceId: string;
 }
 
-
 const SubsribeButton = ({ priceId } : SubsribeButtonProps) => {
-    console.log(priceId)
+    const { data: session } = useSession()
+
+    async function handleSubscribe() {
+        if (!session) {
+          signIn('github')
+          return
+        }
+    
+        try {
+          const response = await api .post('/subscribe')
+    
+          const { sessionId } = response.data
+    
+          const stripe = await getStripeJs()
+    
+          await stripe.redirectToCheckout({ sessionId })
+    
+        } catch (err) {
+          console.log("Erro")
+          alert(err.message)
+        }
+      }
+
     return (
         <button 
             type="button"
+            onClick={handleSubscribe}
             className={styles.subscribeButton}
         >
             Subscribe now
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,14 +20,34 @@ export default NextAuth({
 
     async signIn({ user, account, profile }) {
       const {email} = user
-      await fauna.query(
-        q.Create(
-          q.Collection("users"),
-           { data: { email }}
-        ) 
-      )
-      console.log(user, account, profile)
-      return true
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch (err) {
+        return false
+      }
     },
-  },
+  }
 })
